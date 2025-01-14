801e872cbd7b9b62150136f5a0121fcfce80902a
tjhelpers <sofisk04@gmail.com>
2025-01-14 10:46:29 +0200

    Create methods getOccurrenceCount, getAverageSalary, countTheMonkeys, getHighestAndLowestAge.

--- src/Main.java
+++ src/Main.java
@@ -29,12 +29,34 @@ public class Main {
 //        System.out.println(BasicPractice.removeOddChars("")); // ""
 
 //      MarkStringEnds
-        System.out.println(Arrays.toString(BasicPractice.markStringEnds("hello"))); // ["Hello", "hellO"]
-        System.out.println(Arrays.toString(BasicPractice.markStringEnds("example"))); // ["Example", "examplE"]
-        System.out.println(Arrays.toString(BasicPractice.markStringEnds("Bob"))); // ["Bob", "BoB"]
-        System.out.println(Arrays.toString(BasicPractice.markStringEnds("ALICE"))); // ["ALICE", "ALICE"]
-        System.out.println(Arrays.toString(BasicPractice.markStringEnds("wow!"))); // ["Wow!", "wow!"]
-        System.out.println(Arrays.toString(BasicPractice.markStringEnds("o"))); // ["O", "O"]
+//        System.out.println(Arrays.toString(BasicPractice.markStringEnds("hello"))); // ["Hello", "hellO"]
+//        System.out.println(Arrays.toString(BasicPractice.markStringEnds("example"))); // ["Example", "examplE"]
+//        System.out.println(Arrays.toString(BasicPractice.markStringEnds("Bob"))); // ["Bob", "BoB"]
+//        System.out.println(Arrays.toString(BasicPractice.markStringEnds("ALICE"))); // ["ALICE", "ALICE"]
+//        System.out.println(Arrays.toString(BasicPractice.markStringEnds("wow!"))); // ["Wow!", "wow!"]
+//        System.out.println(Arrays.toString(BasicPractice.markStringEnds("o"))); // ["O", "O"]
+
+
+//      getOccurrenceCount
+//        System.out.println(BasicPractice.getOccurrenceCount(new String[] {"blue", "blue", "blue"}, "blue")); // 3
+//        System.out.println(BasicPractice.getOccurrenceCount(new String[] {"blue", "green", "red"}, "red")); // 1
+//        System.out.println(BasicPractice.getOccurrenceCount(new String[] {"blue", "green", "red"}, "white")); // 0
+//        System.out.println(BasicPractice.getOccurrenceCount(new String[0], "blue")); // 0
+
+//      getAverageSalary
+//        System.out.println(BasicPractice.getAverageSalary(new double[] {1000.00, 500.00})); // 750.00
+//        System.out.println(BasicPractice.getAverageSalary(new double[] {100.00, 200.00, 300.00})); // 200.00
+//        System.out.println(BasicPractice.getAverageSalary(new double[0])); // 0.00
+
+//      getAverageSalary
+        System.out.println(Arrays.toString(BasicPractice.countTheMonkeys(5))); // [1, 2, 3, 4, 5]
+        System.out.println(Arrays.toString(BasicPractice.countTheMonkeys(8))); // [1, 2, 3, 4, 5, 6, 7, 8]
+        System.out.println(Arrays.toString(BasicPractice.countTheMonkeys(0))); // [] (порожній масив)
+
+//      getHighestAndLowestAge
+        System.out.println(Arrays.toString(BasicPractice.getHighestAndLowestAge(new int[]{10, 40, 36}))); // [40, 10]
+        System.out.println(Arrays.toString(BasicPractice.getHighestAndLowestAge(new int[]{60, 4, 14, 20, 97}))); // [97, 4]
+        System.out.println(Arrays.toString(BasicPractice.getHighestAndLowestAge(new int[]{35}))); // [35, 35]
 
     }
 }
--- src/MateTask/BasicPractice.java
+++ src/MateTask/BasicPractice.java
@@ -48,4 +48,73 @@ public class BasicPractice {
             return new String[0];
         }
     }
+
+    /*Реалізуй метод getOccurrenceCount() так, щоби він повертав кількість разів, яку targetColor зустрічається в масиві colors.*/
+    public static int getOccurrenceCount(String[] colors, String targetColor) {
+        int cout = 0;
+        if (colors.length != 0){
+            for ( int i = 0; i < colors.length; i++){
+                if (colors[i].equals(targetColor)){
+                    cout++;
+                }
+            }
+            return cout;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    /*Реалізуй метод getAverageSalary() так, щоби він повертав середнє значення елементів масиву salaries.*/
+    public static double getAverageSalary(double[] salaries) {
+        double sum = 0;
+        if (salaries.length != 0){
+            for (int i = 0; i < salaries.length; i++){
+                sum += salaries[i];
+            }
+            return sum / salaries.length;
+        }
+        else {
+            return 0.0;
+        }
+    }
+
+    /*Реалізуй метод countTheMonkeys() так, щоби він повертав масив типу int, який містить значення від 1 до count. Зверни увагу: якщо count = 0, поверни порожній масив.*/
+    public static int[] countTheMonkeys(int count) {
+        int[] countArray = new int[count];
+        if(count != 0){
+            for(int i = 0; i < count; i++){
+                countArray[i] = i+1;
+            }
+            return countArray;
+        }
+        else {
+            return countArray;
+        }
+    }
+
+    /*Твоє завдання: реалізуй метод getHighestAndLowestAge() так, щоби він повернув масив типу int, який містить найбільший та найменший вік із масиву ages.
+    Зверни увагу: першим значенням елементу нового масиву має бути найбільший вік.*/
+    public static int[] getHighestAndLowestAge(int[] ages) {
+        int[] agesArray = new int[2];
+        int max = ages[0];
+        int min = ages[0];
+        if(ages.length != 0){
+            for (int i = 0; i < ages.length; i++){
+                if (max < ages[i]){
+                    max = ages[i];
+                }
+                if (min > ages[i]){
+                    min = ages[i];
+                }
+            }
+            agesArray[0] = max;
+            agesArray[1] = min;
+            return agesArray;
+        }
+        else {
+            return agesArray;
+        }
+    }
+
 }
